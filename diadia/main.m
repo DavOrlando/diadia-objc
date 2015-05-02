5f1812f08ee35e2c2fabd828773db398608fc596
DavideOrlando <davide.orl94@gmail.com>
2015-05-02 15:59:05 +0200

    Initial Commit

---
+++ diadia/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  diadia
+//
+//  Created by Davide Orlando on 02/05/15.
+//  Copyright (c) 2015 Davide Orlando. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
